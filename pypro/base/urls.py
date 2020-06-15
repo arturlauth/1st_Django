833316b4572c8361985fe4cc42d80d6d1d86cdd3
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-15 11:37:06 -0300

    front end again

--- pypro/base/urls.py
+++ pypro/base/urls.py
@@ -2,11 +2,7 @@ from django.urls import path
 
 from pypro.base.views import home
 
-<<<<<<< HEAD
 app_name = 'base'
-=======
-app_name='base'
->>>>>>> origin/master
 urlpatterns = [
     path('', home, name='home'),
 ]
