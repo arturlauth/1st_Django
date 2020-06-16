ab7a7f771799bcdc343dc855b6383979515a3a5f
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-16 19:38:41 -0300

    Aplicando herança de template e variaveis
    close #68

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -14,7 +14,7 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<h1>Video Motivação:</h1>')
+    assert_contains(resp, '<h1 class="mt-4 mb-3">Video Motivação:</h1>')
 
 
 def test_home_link(resp):
