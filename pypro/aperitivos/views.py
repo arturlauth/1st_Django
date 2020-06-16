e21a6c9c70e7b4194bc06d8b57c7da9f01d17c30
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-16 20:01:04 -0300

    Template de video gerando varias paginas através do uso da tag slug
    close #73

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Motivação:', 'vimeo_id': 429413311}
+    videos = {
+        'demo-spotify': {'titulo': 'Demo Spotify', 'vimeo_id': 6268154},
+        'motivacao': {'titulo': 'Video Motivação:', 'vimeo_id': 429413311}
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
