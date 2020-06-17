0067716596f9ae9e14f1d3460ed09b318a43a992
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-16 21:48:29 -0300

    usando a tag for
    close #75

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 
+videos = [
+        {'slug': 'demo-spotify', 'titulo': 'Demo Spotify:', 'vimeo_id': 6268154},
+        {'slug': 'motivacao', 'titulo': 'Video Motivação:', 'vimeo_id': 429413311}
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
+
 def video(request, slug):
-    videos = {
-        'demo-spotify': {'titulo': 'Demo Spotify', 'vimeo_id': 6268154},
-        'motivacao': {'titulo': 'Video Motivação:', 'vimeo_id': 429413311}
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
