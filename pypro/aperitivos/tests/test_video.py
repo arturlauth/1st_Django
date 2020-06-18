3c8004ceb2f9404a62639e0e488bba3a8332a577
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-18 20:16:11 -0300

    Criação de modelos nos testes
    close #86

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,21 +1,38 @@
 import pytest
 from django.urls import reverse
 
+from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
-    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+def video(db):
+    v = Video(slug='motivacao', titulo='Video Motivação:', vimeo_id='429413311')
+    v.save()
+    return v
+
+
+@pytest.fixture
+def resp(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug,)))
+
+
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(video.slug+'video_nao_existente',)))
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title(resp):
-    assert_contains(resp, '<h1 class="mt-4 mb-3">Video Motivação:</h1>')
+def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 
-def test_home_link(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/429413311"')
+def test_title(resp, video):
+    assert_contains(resp, video.titulo)
+
+
+def test_home_link(resp, video):
+    assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}"')
