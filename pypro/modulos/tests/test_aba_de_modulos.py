60bf468efc13759986a015de6a1811e8d4535716
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-29 14:24:24 -0300

    Migração de dados em um modelo já existente

--- pypro/modulos/tests/test_aba_de_modulos.py
+++ pypro/modulos/tests/test_aba_de_modulos.py
@@ -10,6 +10,7 @@ from pypro.modulos.models import Modulo
 def modulos(db):
     return mommy.make(Modulo, 2)
 
+
 @pytest.fixture
 def resp(client, modulos):
     resp = client.get(reverse('base:home'))
@@ -19,3 +20,8 @@ def resp(client, modulos):
 def test_titulos_dos_modulos(resp, modulos):
     for modulo in modulos:
         assert_contains(resp, modulo.titulo)
+
+
+def test_link_dos_modulos(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.get_absolute_url())
